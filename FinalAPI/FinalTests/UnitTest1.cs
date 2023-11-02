ed09870dc2c976d428aafd3d4402395fdb507b01
Yaroslava Mala <ymala@kse.org.ua>
2023-11-02 09:47:48 +0200

    added unit test

---
+++ FinalAPI/FinalTests/UnitTest1.cs
@@ -0,0 +1,57 @@
+using Moq;
+using Moq.Protected;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Newtonsoft.Json;
+using System.Net;
+using FinalAPI;
+
+namespace FinalAPI.Tests
+{
+    public class FinalUnitTests
+    {
+        private readonly UserService _userService;
+        private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+
+        public FinalUnitTests()
+        {
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            
+            _userService = new UserService(new HttpClient(_mockHttpMessageHandler.Object));
+        }
+
+        [Fact]
+        public async Task GetUsersAsync_ShouldReturnCorrectUsers()
+        {
+            // Arrange
+            var mockData = new UserData
+            {
+                data = new[]
+                {
+                    new UserResponse { Nickname = "TestUser", UserID = "1", FirstSeen = "2023-11-01" }
+                }
+            };
+
+            _mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(JsonConvert.SerializeObject(mockData))
+                });
+
+            // Act
+            var result = (await _userService.GetUsersAsync()).ToList();
+
+            // Assert
+            Assert.Equal(1, result.Count);
+            Assert.Equal("TestUser", result[0].Nickname);
+        }
+    }
+}
